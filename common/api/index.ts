e54c40abe487baccae91c81a2483736d0a092949
543x.com <teabagging80@gmail.com>
2024-07-31 15:26:50 +0800

    Update index.ts

--- common/api/index.ts
+++ common/api/index.ts
@@ -1,18 +1,17 @@
 import { PumpCoin, PumpDetail } from "common/types"
 
+export async function getSolanaTokenList(): Promise<SolscanToken[]> {
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      "token": "HY012210yxj@" // 确保这是有效的Solscan API密钥
+    }
+  };
 export async function getPumpList(): Promise<PumpCoin[]>{
-  const res = await fetch("https://gmgn.ai/defi/quotation/v1/rank/sol/pump?limit=50&orderby=progress&direction=desc&pump=true", {cache: "no-store"})
+  const res = await fetch("https://pro-api.solscan.io/v1.0/token/list?sortBy=market_cap&direction=desc&limit=50", requestOptions)
   const data = await res.json()
   if(data.code === 0){
     return data.data.rank
   }
   return []
-}
-
-export async function getPumpDetail(addr: string): Promise<PumpDetail>{
-  const res = await fetch(`https://gmgn.ai/defi/quotation/v1/tokens/sol/${addr}`)
-  const data = await res.json()
-  if(data.code === 0){
-    return data.data.token
-  }
 }
